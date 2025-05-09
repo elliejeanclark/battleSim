533e866b4c626d7f6be9979b2193a87c243adc4b
Ellie <elinorj@byu.edu>
2025-05-08 19:51:39 -0600

    adjusting combat power in different ships.

--- server/models/Ship.cs
+++ server/models/Ship.cs
@@ -22,9 +22,9 @@ public class Ship {
                 Type = ShipType.Fighter;
                 Health = 50;
                 MaxHealth = 50;
-                ShieldStrength = 100;
-                MaxShieldStrength = 100;
-                Speed = 8;
+                ShieldStrength = 30;
+                MaxShieldStrength = 30;
+                Speed = 7;
                 Size = 1;
                 MissleLaunchers = 2;
                 EnergyWeaponBanks = 3;
@@ -33,64 +33,64 @@ public class Ship {
                 Type = ShipType.Explorer;
                 Health = 100;
                 MaxHealth = 100;
-                ShieldStrength = 180;
-                MaxShieldStrength = 180;
+                ShieldStrength = 120;
+                MaxShieldStrength = 120;
                 Speed = 3;
-                Size = 7;
+                Size = 8;
                 MissleLaunchers = 2;
                 EnergyWeaponBanks = 2;
                 break;
             case "freighter":
                 Type = ShipType.Freighter;
-                Health = 150;
-                MaxHealth = 150;
+                Health = 140;
+                MaxHealth = 140;
                 ShieldStrength = 150;
                 MaxShieldStrength = 150;
                 Speed = 2;
-                Size = 6;
+                Size = 8;
                 MissleLaunchers = 1;
-                EnergyWeaponBanks = 3;
+                EnergyWeaponBanks = 2;
                 break;
             case "cruiser":
                 Type = ShipType.Cruiser;
                 Health = 120;
                 MaxHealth = 120;
-                ShieldStrength = 120;
-                MaxShieldStrength = 120;
+                ShieldStrength = 100;
+                MaxShieldStrength = 100;
                 Speed = 7;
                 Size = 4;
-                MissleLaunchers = 2;
-                EnergyWeaponBanks = 1;
+                MissleLaunchers = 1;
+                EnergyWeaponBanks = 2;
                 break;
             case "destroyer":
                 Type = ShipType.Destroyer;
                 Health = 100;
                 MaxHealth = 100;
-                ShieldStrength = 150;
-                MaxShieldStrength = 150;
+                ShieldStrength = 110;
+                MaxShieldStrength = 110;
                 Speed = 6;
-                Size = 5;
-                MissleLaunchers = 4;
-                EnergyWeaponBanks = 2;
+                Size = 8;
+                MissleLaunchers = 3;
+                EnergyWeaponBanks = 3;
                 break;
             case "carrier":
                 Type = ShipType.Carrier;
                 Health = 200;
                 MaxHealth = 200;
-                ShieldStrength = 200;
-                MaxShieldStrength = 200;
+                ShieldStrength = 100;
+                MaxShieldStrength = 100;
                 Speed = 2;
-                Size = 10;
-                MissleLaunchers = 6;
-                EnergyWeaponBanks = 4;
+                Size = 12;
+                MissleLaunchers = 2;
+                EnergyWeaponBanks = 3;
                 break;
             case "cutter":
                 Type = ShipType.Cutter;
-                Health = 80;
-                MaxHealth = 80;
-                ShieldStrength = 100;
-                MaxShieldStrength = 100;
-                Speed = 9;
+                Health = 100;
+                MaxHealth = 100;
+                ShieldStrength = 80;
+                MaxShieldStrength = 80;
+                Speed = 8;
                 Size = 3;
                 MissleLaunchers = 1;
                 EnergyWeaponBanks = 2;
@@ -102,7 +102,7 @@ public class Ship {
                 ShieldStrength = 50;
                 MaxShieldStrength = 50;
                 Speed = 8;
-                Size = 2;
+                Size = 1;
                 MissleLaunchers = 1;
                 EnergyWeaponBanks = 1;
                 break;
@@ -113,8 +113,8 @@ public class Ship {
                 ShieldStrength = 100;
                 MaxShieldStrength = 100;
                 Speed = 5;
-                Size = 4;
-                MissleLaunchers = 2;
+                Size = 8;
+                MissleLaunchers = 1;
                 EnergyWeaponBanks = 2;
                 break;
             default:
@@ -137,14 +137,14 @@ public class Ship {
 
         switch (weaponType) {
             case WeaponType.Phaser:
-                shieldDamage = 15;
+                shieldDamage = 15 + random.Next(0, 6);
                 dodgeChance -= 2;
                 break;
             case WeaponType.QuantumTorpedoLauncher:
-                hullDamage = 30;
+                hullDamage = 20 + random.Next(5, 11);
                 break;
             case WeaponType.PhotonTorpedoLauncher:
-                hullDamage = 15;
+                hullDamage = 15 + random.Next(0, 6);
                 break;
             case WeaponType.EnergyLance:
                 shieldDamage = 10;
