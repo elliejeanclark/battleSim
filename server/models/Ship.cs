3e9caeb48e6b03ed54c86a05f197c6ffb5e22697
Ellie <elinorj@byu.edu>
2025-05-08 18:30:24 -0600

    finished basic properties

--- server/models/Ship.cs
+++ server/models/Ship.cs
@@ -21,48 +21,72 @@ public class Ship {
                 Health = 100;
                 MaxHealth = 100;
                 ShieldStrength = 100;
+                MaxShieldStrength = 100;
+                Speed = 8;
+                Size = 1;
                 break;
             case "explorer":
                 Type = ShipType.Explorer;
                 Health = 80;
                 MaxHealth = 80;
                 ShieldStrength = 180;
+                MaxShieldStrength = 180;
+                Speed = 3;
+                Size = 7;
                 break;
             case "freighter":
                 Type = ShipType.Freighter;
                 Health = 150;
                 MaxHealth = 150;
                 ShieldStrength = 80;
+                MaxShieldStrength = 80;
+                Speed = 2;
+                Size = 6;
                 break;
             case "cruiser":
                 Type = ShipType.Cruiser;
                 Health = 120;
                 MaxHealth = 120;
                 ShieldStrength = 120;
+                MaxShieldStrength = 120;
+                Speed = 7;
+                Size = 4;
                 break;
             case "destroyer":
                 Type = ShipType.Destroyer;
-                Health = 130;
-                MaxHealth = 130;
+                Health = 120;
+                MaxHealth = 120;
                 ShieldStrength = 200;
+                MaxShieldStrength = 200;
+                Speed = 6;
+                Size = 4;
                 break;
             case "carrier":
                 Type = ShipType.Carrier;
                 Health = 200;
                 MaxHealth = 200;
-                ShieldStrength = 150;
+                ShieldStrength = 200;
+                MaxShieldStrength = 200;
+                Speed = 2;
+                Size = 10;
                 break;
             case "cutter":
                 Type = ShipType.Cutter;
                 Health = 80;
                 MaxHealth = 80;
                 ShieldStrength = 100;
+                MaxShieldStrength = 100;
+                Speed = 9;
+                Size = 3;
                 break;
             case "stealthcraft":
                 Type = ShipType.StealthCraft;
                 Health = 70;
                 MaxHealth = 70;
                 ShieldStrength = 50;
+                MaxShieldStrength = 50;
+                Speed = 8;
+                Size = 2;
                 break;
             case "cargoship":
                 Type = ShipType.CargoShip;
