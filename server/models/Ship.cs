eda4e2b1202f90e6d31268467b653371d27a85d9
Ellie <elinorj@byu.edu>
2025-05-08 18:43:00 -0600

    Starting to include weapon capability

--- server/models/Ship.cs
+++ server/models/Ship.cs
@@ -7,6 +7,8 @@ public class Ship {
     public int MaxShieldStrength { get; set; }
     public int Speed { get; set; }
     public int Size { get; set; }
+    public int MissleLaunchers { get; set; }
+    public int EnergyWeaponBanks { get; set; }
 
     public Ship(string name, string type) {
         Name = name;
@@ -18,30 +20,36 @@ public class Ship {
         switch (type) {
             case "fighter":
                 Type = ShipType.Fighter;
-                Health = 100;
-                MaxHealth = 100;
+                Health = 50;
+                MaxHealth = 50;
                 ShieldStrength = 100;
                 MaxShieldStrength = 100;
                 Speed = 8;
                 Size = 1;
+                MissleLaunchers = 2;
+                EnergyWeaponBanks = 3;
                 break;
             case "explorer":
                 Type = ShipType.Explorer;
-                Health = 80;
-                MaxHealth = 80;
+                Health = 100;
+                MaxHealth = 100;
                 ShieldStrength = 180;
                 MaxShieldStrength = 180;
                 Speed = 3;
                 Size = 7;
+                MissleLaunchers = 2;
+                EnergyWeaponBanks = 2;
                 break;
             case "freighter":
                 Type = ShipType.Freighter;
                 Health = 150;
                 MaxHealth = 150;
-                ShieldStrength = 80;
-                MaxShieldStrength = 80;
+                ShieldStrength = 150;
+                MaxShieldStrength = 150;
                 Speed = 2;
                 Size = 6;
+                MissleLaunchers = 1;
+                EnergyWeaponBanks = 3;
                 break;
             case "cruiser":
                 Type = ShipType.Cruiser;
@@ -51,15 +59,19 @@ public class Ship {
                 MaxShieldStrength = 120;
                 Speed = 7;
                 Size = 4;
+                MissleLaunchers = 2;
+                EnergyWeaponBanks = 1;
                 break;
             case "destroyer":
                 Type = ShipType.Destroyer;
-                Health = 120;
-                MaxHealth = 120;
-                ShieldStrength = 200;
-                MaxShieldStrength = 200;
+                Health = 100;
+                MaxHealth = 100;
+                ShieldStrength = 150;
+                MaxShieldStrength = 150;
                 Speed = 6;
-                Size = 4;
+                Size = 5;
+                MissleLaunchers = 4;
+                EnergyWeaponBanks = 2;
                 break;
             case "carrier":
                 Type = ShipType.Carrier;
@@ -69,6 +81,8 @@ public class Ship {
                 MaxShieldStrength = 200;
                 Speed = 2;
                 Size = 10;
+                MissleLaunchers = 6;
+                EnergyWeaponBanks = 4;
                 break;
             case "cutter":
                 Type = ShipType.Cutter;
@@ -78,6 +92,8 @@ public class Ship {
                 MaxShieldStrength = 100;
                 Speed = 9;
                 Size = 3;
+                MissleLaunchers = 1;
+                EnergyWeaponBanks = 2;
                 break;
             case "stealthcraft":
                 Type = ShipType.StealthCraft;
@@ -87,12 +103,19 @@ public class Ship {
                 MaxShieldStrength = 50;
                 Speed = 8;
                 Size = 2;
+                MissleLaunchers = 1;
+                EnergyWeaponBanks = 1;
                 break;
             case "cargoship":
                 Type = ShipType.CargoShip;
                 Health = 150;
                 MaxHealth = 150;
                 ShieldStrength = 100;
+                MaxShieldStrength = 100;
+                Speed = 5;
+                Size = 4;
+                MissleLaunchers = 2;
+                EnergyWeaponBanks = 2;
                 break;
             default:
                 throw new ArgumentException("Invalid ship type");
