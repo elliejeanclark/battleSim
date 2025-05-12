1cf85068f3d42359dd49019b5762094f295d26a6
Ellie <elinorj@byu.edu>
2025-05-12 12:36:40 -0600

    starting to implement critical hits

--- server/models/Battle.cs
+++ server/models/Battle.cs
@@ -29,11 +29,15 @@ public class Battle {
                     Console.WriteLine($"Unable to fire {weapon.Type} because it is on cooldown.");
                 }
                 else {
+                    if (CheckNotCompleted() == false) {
+                        return;
+                    }
                     Attack(attacker, defender, weapon);
                     weapon.Cooldown = weapon.FireRate;
                     usedWeapons.Add(weapon);
                 }
             }
+            
             foreach (Weapon weapon in attacker.MissleWeapons) {
                 if (!usedWeapons.Contains(weapon)) {
                     if (weapon.Cooldown > 0) {
@@ -52,9 +56,18 @@ public class Battle {
         }
     }
 
+    private Boolean CheckNotCompleted() {
+        if (Status == "In Progress") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     private void Attack(Ship attacker, Ship defender, Weapon weapon) {
-        int shieldDamage = weapon.ShieldDamage;
-        int hullDamage = weapon.HullDamage;
+        int shieldDamage = 0;
+        int hullDamage = 0;
         Random random = new Random();
 
         int dodgeChance = random.Next(1, 21);
@@ -65,33 +78,43 @@ public class Battle {
         else {
             dodgeChance -= (defender.Size - 1) / 2;
         }
-
         if (dodgeChance > 15) {
             Console.WriteLine($"{defender.Name} dodged the attack!");
             return;
         }
+
+        int critChance = random.Next(1, 11);
+        if (critChance >= weapon.CriticalChance) {
+            shieldDamage += (int)(weapon.ShieldDamage * weapon.CriticalMultiplier);
+            hullDamage += (int)(weapon.HullDamage * weapon.CriticalMultiplier);
+            Console.WriteLine($"{attacker.Name} scored a critical hit!");
+        }
         else {
-            if (defender.ShieldStrength > 0 && shieldDamage > 0) {
-                defender.ShieldStrength -= shieldDamage;
-                if (defender.ShieldStrength < 0) {
-                    defender.ShieldStrength = 0;
-                    Console.WriteLine($"{defender.Name}'s shields are down!");
+            shieldDamage = weapon.ShieldDamage;
+            hullDamage = weapon.HullDamage;
+        }
+
+
+        if (defender.ShieldStrength > 0 && shieldDamage > 0) {
+            defender.ShieldStrength -= shieldDamage;
+            if (defender.ShieldStrength < 0) {
+                defender.ShieldStrength = 0;
+                Console.WriteLine($"{defender.Name}'s shields are down!");
+            }
+            else {
+                Console.WriteLine($"{defender.Name}'s shields took {shieldDamage} damage!");
+            }
+        }
+        else if (defender.ShieldStrength == 0) {
+            if (defender.Health > 0  && hullDamage > 0) {
+                defender.Health -= hullDamage;
+                if (defender.Health < 0) {
+                    defender.Health = 0;
+                    Console.WriteLine($"{defender.Name} been destroyed!");
+                    EndBattle(attacker);
                 }
                 else {
-                    Console.WriteLine($"{defender.Name}'s shields took {shieldDamage} damage!");
-                }
-            }
-            else if (defender.ShieldStrength == 0) {
-                if (defender.Health > 0  && hullDamage > 0) {
-                    defender.Health -= hullDamage;
-                    if (defender.Health < 0) {
-                        defender.Health = 0;
-                        Console.WriteLine($"{defender.Name} been destroyed!");
-                        EndBattle(attacker);
-                    }
-                    else {
-                        Console.WriteLine($"{defender.Name} took {hullDamage} damage to hull!");
-                    }
+                    Console.WriteLine($"{defender.Name} took {hullDamage} damage to hull!");
                 }
             }
         }
