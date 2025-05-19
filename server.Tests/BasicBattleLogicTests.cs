63174fd8279625b7d13e047af1b2be23daa5eac8
Ellie <elinorj@byu.edu>
2025-05-19 16:29:45 -0600

    renaming test files

---
+++ server.Tests/BasicBattleLogicTests.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Xunit;
+using BattleSim.Server.Models;
+using BattleSim.Server.Enums;
+using BattleSim.Server.Exceptions;
+
+namespace Server.Tests {
+    public class BasicBattleLogicTests
+    {
+
+    }
+}
